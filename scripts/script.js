8d79ef8358452b5d7ab8bf0fed2336200b2ad656
Robert Ruse <robert.a.ruse@gmail.com>
2020-12-25 02:20:21 +0200

    Refactor script.js

--- scripts/script.js
+++ scripts/script.js
@@ -7,15 +7,16 @@ const roundElement = document.querySelector("#round");
 const rezultElement = document.querySelector("#rezult");
 const dialUserElement = document.querySelector("#dial-user > div");
 const dialCompElement = document.querySelector("#dial-comp > div");
-const choicesElement = document.querySelectorAll(".choice");
+const choiceElements = document.querySelectorAll(".choice");
 const resetElement = document.querySelector("#reset");
-const dialIconsUserElement = document.querySelectorAll(
+const dialIconUserElements = document.querySelectorAll(
   ".container-icons-user .dial-icon"
 );
-const dialIconsCompElement = document.querySelectorAll(
+const dialIconCompElements = document.querySelectorAll(
   ".container-icons-comp .dial-icon"
 );
 
+/* Generate computer choice */
 function getCompChoice() {
   const choices = ["rock", "paper", "scissors"];
   const randomNumber = Math.floor(Math.random() * 3);
@@ -23,7 +24,6 @@ function getCompChoice() {
 }
 
 /* Generate winnig message */
-
 function generateMessage(winningChoice) {
   let message = "";
   switch (winningChoice) {
@@ -60,148 +60,90 @@ function draw(userChoice, compChoice) {
   rezultElement.innerText = `${userChoice} ties ${compChoice}.`;
 }
 
+function setGameState(userChoice, compChoice) {
+  switch (`${userChoice}-${compChoice}`) {
+    case "rock-scissors":
+    case "paper-rock":
+    case "scissors-paper":
+      win(userChoice);
+      break;
+    case "rock-paper":
+    case "paper-scissors":
+    case "scissors-rock":
+      lose(compChoice);
+      break;
+    default:
+      draw(userChoice, compChoice);
+  }
+}
+
 /* Rotate Dial */
-function addRotateDialUser(userChoice) {
-  removeRotateDialUser();
-  dialUserElement.classList.add(`dial-rotate-${userChoice}`);
+function addRotateDial(dialElement, choice) {
+  removeRotateDial(dialElement);
+  dialElement.classList.add(`dial-rotate-${choice}`);
 }
 
-function removeRotateDialUser() {
-  dialUserElement.classList.remove("dial-rotate-rock");
-  dialUserElement.classList.remove("dial-rotate-paper");
-  dialUserElement.classList.remove("dial-rotate-scissors");
+function removeRotateDial(dialElement) {
+  dialElement.classList.remove(
+    "dial-rotate-rock",
+    "dial-rotate-paper",
+    "dial-rotate-scissors"
+  );
 }
 
-function addRotateDialComp(compChoice) {
-  removeRotateDialComp();
-  dialCompElement.classList.add(`dial-rotate-${compChoice}`);
-}
-
-function removeRotateDialComp() {
-  dialCompElement.classList.remove("dial-rotate-rock");
-  dialCompElement.classList.remove("dial-rotate-paper");
-  dialCompElement.classList.remove("dial-rotate-scissors");
-}
-
-/* Add dial icon color */
-function addDialIconColorUser(userChoice) {
-  removeIconColorUser();
-  dialIconsUserElement.forEach((item) => {
-    if (item.id === `icon-${userChoice}`) {
-      item.classList.add("icon-color");
-    }
+/* Change dial icon color */
+function addDialIconColor(dialIconElements, choice) {
+  removeIconColor(dialIconElements);
+  dialIconElements.forEach((item) => {
+    if (item.id !== `icon-${choice}`) return;
+    item.classList.add("icon-color");
   });
 }
 
-function removeIconColorUser() {
-  dialIconsUserElement.forEach((item) => {
-    item.classList.remove("icon-color");
-  });
-}
-
-function addDialIconColorComp(compChoice) {
-  removeIconColorComp();
-  dialIconsCompElement.forEach((item) => {
-    if (item.id === `icon-${compChoice}`) {
-      item.classList.add("icon-color");
-    }
-  });
-}
-
-function removeIconColorComp() {
-  dialIconsCompElement.forEach((item) => {
+function removeIconColor(dialIconElements) {
+  dialIconElements.forEach((item) => {
     item.classList.remove("icon-color");
   });
 }
 
 /* Move Slider */
-function addMoveSliderUser() {
-  switch (userScore) {
+function addMoveSlider(sliderElement, score) {
+  switch (score) {
     case 1:
-      sliderUserElement.classList.add("slider-1");
+      sliderElement.classList.add("slider-1");
       break;
     case 2:
-      sliderUserElement.classList.add("slider-2");
+      sliderElement.classList.add("slider-2");
       break;
     case 3:
-      sliderUserElement.classList.add("slider-3");
+      sliderElement.classList.add("slider-3");
   }
 }
 
-function removeMoveSliderUser() {
-  sliderUserElement.classList.remove("slider-1");
-  sliderUserElement.classList.remove("slider-2");
-  sliderUserElement.classList.remove("slider-3");
-}
-
-function addMoveSliderComp() {
-  switch (compScore) {
-    case 1:
-      sliderCompElement.classList.add("slider-1");
-      break;
-    case 2:
-      sliderCompElement.classList.add("slider-2");
-      break;
-    case 3:
-      sliderCompElement.classList.add("slider-3");
-  }
-}
-
-function removeMoveSliderComp() {
-  sliderCompElement.classList.remove("slider-1");
-  sliderCompElement.classList.remove("slider-2");
-  sliderCompElement.classList.remove("slider-3");
+function removeMoveSlider(sliderElement) {
+  sliderElement.classList.remove("slider-1", "slider-2", "slider-3");
 }
 
 /* Game over */
 function gameOver() {
-  roundElement.innerText = "Game over";
-  if (userScore > compScore) {
-    rezultElement.innerText = "You win!";
-  } else {
-    rezultElement.innerText = "You lose!";
-  }
-}
-
-function gameOver() {
-  if (userScore > compScore) {
-    roundElement.innerText = "Game over - You win!";
-  } else {
-    roundElement.innerText = "Game over - You lose!";
-  }
+  roundElement.innerText =
+    userScore > compScore ? "Game over - You win!" : "Game over - You lose!";
 }
 
 /* Game round */
 function game(e) {
-  if (userScore < 3 && compScore < 3) {
-    const userChoice = e.currentTarget.id;
-    const compChoice = getCompChoice();
-
-    switch (`${userChoice}-${compChoice}`) {
-      case "rock-scissors":
-      case "paper-rock":
-      case "scissors-paper":
-        win(userChoice);
-        break;
-      case "rock-paper":
-      case "paper-scissors":
-      case "scissors-rock":
-        lose(compChoice);
-        break;
-      default:
-        draw(userChoice, compChoice);
-    }
-
-    addDialIconColorUser(userChoice);
-    addDialIconColorComp(compChoice);
-    addRotateDialUser(userChoice);
-    addRotateDialComp(compChoice);
-    addMoveSliderUser();
-    addMoveSliderComp();
-
-    if (userScore == 3 || compScore == 3) {
-      gameOver();
-    }
+  if (userScore >= 3 || compScore >= 3) return;
+  const userChoice = e.currentTarget.id;
+  const compChoice = getCompChoice();
+  setGameState(userChoice, compChoice);
+  addDialIconColor(dialIconUserElements, userChoice);
+  addDialIconColor(dialIconCompElements, compChoice);
+  addRotateDial(dialUserElement, userChoice);
+  addRotateDial(dialCompElement, compChoice);
+  addMoveSlider(sliderUserElement, userScore);
+  addMoveSlider(sliderCompElement, compScore);
+  if (userScore == 3 || compScore == 3) {
+    gameOver();
   }
 }
 
@@ -210,22 +152,21 @@ function reset() {
   round = 0;
   userScore = 0;
   compScore = 0;
-  removeIconColorUser();
-  removeIconColorComp();
-  removeRotateDialUser();
-  removeRotateDialComp();
-  removeMoveSliderUser();
-  removeMoveSliderComp();
+  removeIconColor(dialIconUserElements);
+  removeIconColor(dialIconCompElements);
+  removeRotateDial(dialUserElement);
+  removeRotateDial(dialCompElement);
+  removeMoveSlider(sliderUserElement);
+  removeMoveSlider(sliderCompElement);
   roundElement.innerText = "Rock, Paper, Scissors...";
   rezultElement.innerText = "Shoot!";
 }
 
 /* Main */
 function main() {
-  choicesElement.forEach((item) => {
+  choiceElements.forEach((item) => {
     item.addEventListener("click", game);
   });
-
   resetElement.addEventListener("click", reset);
 }
 
